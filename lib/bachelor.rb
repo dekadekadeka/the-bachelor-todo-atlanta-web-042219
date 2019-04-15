ecc1158d9648f6e54ecd83e34621d7e4ee961292
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-04-15 15:58:18 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -47,7 +47,7 @@ def get_average_age_for_season(data, season)
   data[season].each do |contestant|
     ages = contestant["age"].to_f
   #binding.pry
-    sum += ages.round
+    sum += ages
   end
   return average = (sum / data[season].length)
 end
